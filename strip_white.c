bbe00422477f208a3ca982585ff2751d0de6b692
soumya mandi <soumyamandi@gmail.com>
2012-11-09 01:13:08 +0530

    done with strip white

--- strip_white.c
+++ strip_white.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* strip_white( char* str ) {
+	int len = strlen( str ) ;
+	char* res = ( char* )malloc( sizeof( char ) * len ) ;
+	int res_index = 0;
+	//remove first bunch
+	int i = 0 ;
+	while( *(str+i) == ' ' ){
+		i++ ;
+	}
+
+	//Now i has the first non-whitespace character
+	for( ; i < len ;i++ ){
+		if( *(str+i) != ' ' ){
+			res[ res_index++ ] = *(str+i) ;
+		}
+		else {
+			while( *(str+i) == ' ' &&  i < len ){
+				i++ ;
+			}
+			if( i == len ){
+				return res ;
+			}
+			else {
+				i-- ;
+				res[ res_index++ ] = ' ' ;
+			}
+		}
+	}
+	return res ;
+
+
+}
 
 int main( int main,char** argc ) {
+	
+	char input[] = "my      name   is soumya mandi                " ;
 
+	char* res = strip_white( input ) ;
+	printf("res is :%s<END>\n", res ) ;
 
 
 
